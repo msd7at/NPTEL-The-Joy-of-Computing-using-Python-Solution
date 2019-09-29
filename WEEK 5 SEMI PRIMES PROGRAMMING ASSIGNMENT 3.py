b0df93ac92fb56b09ca829993939c01a9a70bbfa
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2019-09-29 21:00:22 +0530

    Update and rename WEEK 5 SEMI PRIMES PROGRAMMING ASSIGNMENT 3 to WEEK 5 SEMI PRIMES PROGRAMMING ASSIGNMENT 3.py

--- WEEK 5 SEMI PRIMES PROGRAMMING ASSIGNMENT 3.py
+++ WEEK 5 SEMI PRIMES PROGRAMMING ASSIGNMENT 3.py
@@ -1,4 +1,4 @@
-QUESTION
+"""QUESTION
 
 A semiprime number is an integer which can be expressed as a product of two distinct primes. For example 15 = 3*5 is a semiprime number but 9 = 3*3 is not .
 Given an integer number N, find whether it can be expressed as a sum of two semi-primes or not (not necessarily distinct).
@@ -22,7 +22,7 @@ N = 30 can be expressed as 15+15 where 15 is a semi-prime number (5*3 = 15)
 
 NOTE: N is less than equal to 200
 
-SOLUTION
+SOLUTION """
 
 prime1=[]
 prime2=[]
