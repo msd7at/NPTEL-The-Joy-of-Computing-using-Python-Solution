c70677c9decfb261200444fd80d75d8abcf9d423
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2019-09-29 20:59:17 +0530

    Update and rename WEEK 6 DICTIONARY PROGRAMMING ASSIGNMENT 2 to WEEK 6 DICTIONARY PROGRAMMING ASSIGNMENT 2.py

--- WEEK 6 DICTIONARY PROGRAMMING ASSIGNMENT 2.py
+++ WEEK 6 DICTIONARY PROGRAMMING ASSIGNMENT 2.py
@@ -1,4 +1,4 @@
-QUESTION
+"""QUESTION
 Given a positive integer number n, you have to write a program that generates a dictionary d which contains (i, i*i*i) such that i is the key and i*i*i is its value, where i is from 1 to n (both included).
 Then you have to just print this dictionary d.
 
@@ -28,7 +28,7 @@ Explanation:
 Here n is 8, we will start from i=1, hence the first element of the dictionary is (1: 1), as i becomes 2, the second element of the dictionary becomes (2: 8) and so on.
 Hence the output will be {1: 1, 2: 8, 3: 27, 4: 64, 5: 125, 6: 216, 7: 343, 8: 512}.
 
-SOLUTION
+SOLUTION"""
 
 n=int(input())
 d={}
